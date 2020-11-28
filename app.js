99a859c02df1ccd9716439ee808d2cd9571b07d8
iamsaif <saifulhasan580@gmail.com>
2020-11-28 22:42:23 +0530

    bug fixes

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 require("dotenv").config();
 const express=require('express');
 const app=express();
+const userRouter=require("./api/users/user.router")
 
-app.get('/',(req,res)=>{
-    res.send("SAIF")
-});
-app.get('/api',(req,res)=>{
-    res.json({
-        success:1,
-        message:"Working"
-    });
-});
+// app.get('/',(req,res)=>{
+//     res.send("SAIF")
+// });
+// app.get('/api',(req,res)=>{
+//     res.json({
+//         success:1,
+//         message:"Working"
+//     });
+// });
+app.use(express.json());
+app.use("/api/users",userRouter);
 
 app.listen(process.env.APP_PORT,()=>{
     console.log("Server is runnning");
